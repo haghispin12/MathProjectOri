f548af4a1d848ae9b9c6b36e0154f832b39cb1d6
user <hag.hispin12@gmail.com>
2025-09-14 15:34:39 +0300

    v1

---
+++ app/src/main/java/com/example/mathporjectori/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mathporjectori;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    private Button buttonup10;
+    private Button buttonup20;
+
+    private Button buttonexsercise;
+    private Button buttoncheck;
+    private Button  Etenswer ;
+    private Button TVseconednum;
+    private Button  TVfirstnum;
+
+
+
+}
